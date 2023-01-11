5985016da499d8f40384af8e1b2657f7c721afd5
lewns2 <lewns2002@naver.com>
2023-01-11 17:59:27 +0900

    init

---
+++ backend/src/test/java/com/lewns2/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewns2.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
