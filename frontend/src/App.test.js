e95709679355303a6d1736bc40a0646a2b8f1d36
lewns2 <lewns2002@naver.com>
2023-01-27 19:09:30 +0900

    [init] 폴더 구조

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('APP_컴포넌트_테스트 ', () => {
+  test('', async () => {
+    render(<App />);
+
+    // something test...
+  });
 });
