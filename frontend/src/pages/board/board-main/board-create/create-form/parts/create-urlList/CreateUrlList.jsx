cf8064b26fbab0b49311625cf49e6a90a55b2d38
lewns2 <lewns2002@naver.com>
2023-02-20 18:47:03 +0900

    add: board-form

--- frontend/src/pages/board/board-main/board-create/create-form/parts/create-urlList/CreateUrlList.jsx
+++ frontend/src/pages/board/board-main/board-create/create-form/parts/create-urlList/CreateUrlList.jsx
@@ -6,11 +6,19 @@ const CreateUrlList = () => {
 
   return (
     <div>
-      <span onClick={() => setSize((prev) => prev + 1)}>+ 추가</span>
-      <span onClick={() => setSize((prev) => (prev < 1 ? prev : prev - 1))}>- 삭제</span>
-      {size}
+      <HandleUrlSize setSize={setSize} />
       <CreateUrl size={size} />
     </div>
+  );
+};
+
+// Component : URL 데이터들의 갯수 조절
+const HandleUrlSize = (props) => {
+  return (
+    <>
+      <span onClick={() => props.setSize((prev) => prev + 1)}>+ 추가</span>
+      <span onClick={() => props.setSize((prev) => (prev < 1 ? prev : prev - 1))}>- 삭제</span>
+    </>
   );
 };
 
