7b1da9615dca78a57405bb9210265b078ee26dbb
lewns2 <lewns2002@naver.com>
2023-02-21 17:52:45 +0900

    [FE] fix: Single URL Input을 제어 컴포넌트에서 비제어 컴포넌트로 변경

--- frontend/src/pages/board/board-main/board-create/create-form/parts/create-urlList/create-url/CreateUrl.jsx
+++ frontend/src/pages/board/board-main/board-create/create-form/parts/create-urlList/create-url/CreateUrl.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useRef } from 'react';
 
 import { useBoardCreateContext } from '../../../hooks';
 
@@ -20,23 +20,27 @@ const CreateUrl = (props) => {
   );
 };
 
-// Component : 하나의 URL 인풋 데이터 관리 컴포넌트 => 문제점 : state 변경에 따른 input 한글자 후 focus 잃음.
+// Component : 하나의 URL 인풋 데이터 관리 컴포넌트 => 문제점 : state 변경에 따른 input 한글자 후 focus 잃음. => 비제어 컴포넌트로 변경
 const SingleUrlData = (props) => {
-  const [url, setUrl] = useState({ address: '', category: '' });
+  const url = {
+    address: '',
+    category: '',
+  };
+  const addressInputRef = useRef();
+  const categoryInputRef = useRef();
 
-  const handleData = (e) => {
-    const { name, value } = e.target;
-    setUrl({ ...url, [name]: value || '' });
-    console.log(url);
+  const handleData = () => {
+    url.address = addressInputRef.current.value;
+    url.category = categoryInputRef.current.value;
     props.urlArr[props.index] = url;
   };
 
   return (
     <Fragment>
       <label>URL 주소 : </label>
-      <input onChange={(e) => handleData(e)} name="address"></input>
+      <input onChange={() => handleData()} name="address" ref={addressInputRef}></input>
       <label> 카테고리 : </label>
-      <input onChange={(e) => handleData(e)} name="category"></input>
+      <input onChange={() => handleData()} name="category" ref={categoryInputRef}></input>
       <button>삭제</button>
     </Fragment>
   );
